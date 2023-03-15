e549aebbbc019daf5971da4cd520f96f92454e0e
VuNAP <vuvovan2k2>
2023-03-15 21:47:57 +0700

    [EDIT] edit border

--- lib/views/widgets/text_field_common.dart
+++ lib/views/widgets/text_field_common.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo_list/utils/color_utils.dart';
 
 class TextFieldCommon extends TextField {
   TextFieldCommon({
@@ -9,32 +10,32 @@ class TextFieldCommon extends TextField {
     super.maxLines,
     super.maxLength,
     super.obscureText,
+    super.textAlign,
     String? hintText,
     TextStyle? hintStyle,
     int? hintMaxLines,
     Widget? prefixIcon,
     Widget? suffixIcon,
     EdgeInsetsGeometry? contentPadding,
+    InputBorder? border,
     InputBorder? focusedBorder,
     InputBorder? errorBorder,
     InputBorder? enabledBorder,
     InputBorder? disabledBorder,
   }) : super(
           decoration: InputDecoration(
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(10),
-            ),
-            focusedBorder: focusedBorder,
-            errorBorder: errorBorder,
-            enabledBorder: enabledBorder,
-            disabledBorder: disabledBorder,
+            border: border ?? OutlineInputBorder(borderRadius: BorderRadius.circular(10), borderSide: const BorderSide(color: ColorUtils.greyCE)),
+            focusedBorder: focusedBorder ?? OutlineInputBorder(borderRadius: BorderRadius.circular(10), borderSide: const BorderSide(color: ColorUtils.greyCE)),
+            errorBorder: errorBorder ?? OutlineInputBorder(borderRadius: BorderRadius.circular(10), borderSide: const BorderSide(color: ColorUtils.greyCE)),
+            enabledBorder: enabledBorder ?? OutlineInputBorder(borderRadius: BorderRadius.circular(10), borderSide: const BorderSide(color: ColorUtils.greyCE)),
+            disabledBorder: disabledBorder ?? OutlineInputBorder(borderRadius: BorderRadius.circular(10), borderSide: const BorderSide(color: ColorUtils.greyCE)),
             hintText: hintText,
             hintStyle: hintStyle,
             hintMaxLines: hintMaxLines,
             prefixIcon: prefixIcon,
             suffixIcon: suffixIcon,
             contentPadding: contentPadding,
+            counter: const Offstage(),
           ),
-
         );
 }
