7913b07a5f53075f6f05a54d03892ae21721f63f
VuNAP <vuvovan2k2>
2023-03-16 23:32:08 +0700

    [EDIT] config tab bar

--- lib/views/home/home_screen.dart
+++ lib/views/home/home_screen.dart
@@ -4,6 +4,7 @@ import 'package:todo_list/languages/language.dart';
 import 'package:todo_list/utils/color_utils.dart';
 import 'package:todo_list/utils/format_utils.dart';
 import 'package:todo_list/utils/text_style_utils.dart';
+import 'package:todo_list/views/widgets/search_bar_common.dart';
 
 import '../../route/route.gr.dart';
 
@@ -14,83 +15,106 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: AutoTabsScaffold(
-        routes: const [
-          DashboardScreenRoute(),
-          ProjectScreenRoute(),
-        ],
-        appBarBuilder: (context, tabsRouter) => PreferredSize(
-          preferredSize: const Size.fromHeight(120),
-          child: Container(
-            color: ColorUtils.bgColor,
-            padding: const EdgeInsets.all(16),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                // TODO: split component
-                Row(
-                  mainAxisSize: MainAxisSize.max,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        // TODO: implement avatar
-                        Icon(Icons.person_2_outlined),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                          child: Text(
-                            "Morning, VuNA",
-                            style: TextStyleUtils.textStyleOpenSans20W400,
-                          ),
-                        ),
-                        // TODO: implement icon
-                        Icon(Icons.waving_hand_rounded),
-                      ],
-                    ),
-                    // TODO: implement icon
-                    Icon(Icons.notifications_active_outlined),
-                  ],
-                ),
-                if (tabsRouter.current.name == DashboardScreenRoute.name) ...[
-                  Text(
-                    L.current.todayLabel,
-                    style: TextStyleUtils.textStyleOpenSans22W400,
-                  ),
-                  Text(
-                    FormatUtils.dateFormat.format(DateTime.now()),
-                    style: TextStyleUtils.textStyleOpenSans20W400,
-                  ),
-                ] else if(tabsRouter.current.name == ProjectScreenRoute.name) ...[
-                  Text(
-                    L.current.projectLabel,
-                    style: TextStyleUtils.textStyleOpenSans22W400,
-                  ),
-                ]
-              ],
-            ),
-          ),
-        ),
-        bottomNavigationBuilder: (_, tabsRouter) {
-          return BottomNavigationBar(
-            currentIndex: tabsRouter.activeIndex,
-            onTap: tabsRouter.setActiveIndex,
-            items: const [
-              BottomNavigationBarItem(
-                label: 'Home',
-                icon: Icon(
-                  Icons.home,
-                ),
-              ),
-              BottomNavigationBarItem(
-                label: 'Project',
-                icon: Icon(
-                  Icons.folder,
-                ),
-              ),
-            ],
-          );
+      child: GestureDetector(
+        onTap: () {
+          FocusManager.instance.primaryFocus?.unfocus();
         },
+        child: AutoTabsScaffold(
+          routes: const [
+            DashboardScreenRoute(),
+            ProjectScreenRoute(),
+          ],
+          appBarBuilder: (context, tabsRouter) => PreferredSize(
+            preferredSize: Size.fromHeight(_getHeightAppBar(tabsRouter)),
+            child: _appBar(context, tabsRouter),
+          ),
+          bottomNavigationBuilder: (_, tabsRouter) {
+            return BottomNavigationBar(
+              currentIndex: tabsRouter.activeIndex,
+              onTap: tabsRouter.setActiveIndex,
+              items: const [
+                BottomNavigationBarItem(
+                  label: 'Home',
+                  icon: Icon(
+                    Icons.home,
+                  ),
+                ),
+                BottomNavigationBarItem(
+                  label: 'Project',
+                  icon: Icon(
+                    Icons.folder,
+                  ),
+                ),
+              ],
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  double _getHeightAppBar(TabsRouter tabsRouter) {
+    if(tabsRouter.current.name == DashboardScreenRoute.name) {
+      return 100;
+    } else if(tabsRouter.current.name == ProjectScreenRoute.name) {
+      return 145;
+    } else {
+      return 50;
+    }
+  }
+
+  Widget _appBar(BuildContext context, TabsRouter tabsRouter) {
+    return Container(
+      color: ColorUtils.bgColor,
+      padding: const EdgeInsets.fromLTRB(16,16,16,0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // TODO: split component
+          Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  // TODO: implement avatar
+                  Icon(Icons.person_2_outlined),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: Text(
+                      "Morning, VuNA",
+                      style: TextStyleUtils.textStyleOpenSans20W400,
+                    ),
+                  ),
+                  // TODO: implement icon
+                  Icon(Icons.waving_hand_rounded),
+                ],
+              ),
+              // TODO: implement icon
+              Icon(Icons.notifications_active_outlined),
+            ],
+          ),
+          if (tabsRouter.current.name == DashboardScreenRoute.name) ...[
+            Text(
+              L.current.todayLabel,
+              style: TextStyleUtils.textStyleOpenSans22W400,
+            ),
+            Text(
+              FormatUtils.dateFormat.format(DateTime.now()),
+              style: TextStyleUtils.textStyleOpenSans20W400,
+            ),
+          ] else if(tabsRouter.current.name == ProjectScreenRoute.name) ...[
+            Text(
+              L.current.projectLabel,
+              style: TextStyleUtils.textStyleOpenSans22W400,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 12.0),
+              child: SearchBarCommon(),
+            ),
+          ]
+        ],
       ),
     );
   }
